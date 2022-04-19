8f1fd4e5d12adf1892252e351fd6179f9ab6ebce
Joao Tiburcio <corp.comission@gmail.com>
2022-04-19 12:10:09 -0300

    LamaDev frontend e backend

--- server/src/routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-import HelloController from "./controllers/HelloController";
-
-const routes = new Router();
-
-routes.get('/hello', HelloController.index);
-
-export default routes;
