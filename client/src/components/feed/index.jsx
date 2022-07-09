0f15394bc92f3c0f7b510c6a6264f14fb60d439c
Joao Tiburcio <corp.comission@gmail.com>
2022-07-09 13:41:15 -0300

    Unindo client e server

---
+++ client/src/components/feed/index.jsx
@@ -0,0 +1,39 @@
+import React, { useContext ,useEffect, useState } from 'react';
+import { axiosInstance } from "../../config";
+import "./styles.css"
+
+import { AuthContext } from "../../context/AuthContext";
+
+import Share from '../share';
+import Post from '../post';
+
+function Feed({username}) {
+  const [posts, setPosts] = useState([]);
+  const {user} = useContext(AuthContext);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = username 
+      ? await axiosInstance.get("/posts/profile/"+ username) 
+      : await axiosInstance.get("posts/timeline/"+ user._id);
+      setPosts(
+        res.data.sort((p1, p2) => {
+          return new Date(p2.createdAt) - new Date(p1.createdAt);
+        }));
+    }
+    fetchPosts();
+  }, [username, user._id]);
+
+  return (
+      <div className='Feed'>
+          <div className="feedWrapper">
+            {(!username || username === user.username) && <Share/>}
+            { posts.map((p) => (
+              <Post key={p._id} post={p}/>
+            ))}
+          </div>
+      </div>
+  );
+}
+
+export default Feed;
