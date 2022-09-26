34a12985663233978a6dcf92b74c17fbe0d1ca20
JMTiburcio <jmrtme@gmail.com>
2022-09-26 18:14:09 -0300

    adding yarn.lock on .gitignore and minor change on jobs page

--- client/src/pages/jobs/index.jsx
+++ client/src/pages/jobs/index.jsx
@@ -25,7 +25,6 @@ function Jobs() {
           res.data.sort((p1, p2) => {
             return new Date(p2.createdAt) - new Date(p1.createdAt);
           }));
-          console.log(res.data)
       }
       fetchJobs();
     }, []);
