4d4b4bb31c357176e055da3a62b4cbe921247b23
JMTiburcio <jmrtme@gmail.com>
2022-09-29 20:07:35 -0300

    removing the changes that I have made

--- routes/users.js
+++ routes/users.js
@@ -40,7 +40,7 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
-//get a user
+//get a user Problem with this route
 router.get("/", async (req, res) => {
   const userId = req.query.userId;
   const username = req.query.username;
