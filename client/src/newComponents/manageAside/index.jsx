610458e5ed2e21fabff16c63ff3444dac2ea68d4
JMTiburcio <jmrtme@gmail.com>
2022-10-10 19:06:20 -0300

    creating the new components for the manageJob page

---
+++ client/src/newComponents/manageAside/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './styles.css';
+
+function ManageAside({ }) {
+  return (
+    <aside>
+      <a href="#">Post a free job</a>
+      <div>
+        <p>Save up to 35% by purchasing job posting budget in advance.</p>
+        <a href="#">Get discount</a>
+      </div>
+      <div>
+        <p>Billing information</p>
+        <a href="#">Payment method</a>
+        <a href="#">Purchase history</a>
+      </div>
+    </aside>
+  );
+}
+
+export default ManageAside;
---
+++ client/src/newComponents/manageMenu/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+
+function ManageMenu({ }) {
+  return (
+    <section className='manageMenu'>
+        <h2>My item</h2>
+        <div><a href="">Posted Jobs</a></div>
+        <div><a href="">My Jobs</a></div>
+    </section>
+  );
+}
+
+export default ManageMenu;
