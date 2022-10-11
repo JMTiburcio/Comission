77bb59ea4f3f6c98c7b11f3d20d0a70c5131cfbe
JMTiburcio <jmrtme@gmail.com>
2022-10-11 19:42:17 -0300

    debugging manageJob

--- client/src/pages/manageJob/index.jsx
+++ client/src/pages/manageJob/index.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import './styles.css';
-import { useState, useContext } from 'react';
 import { AuthContext } from "../../context/AuthContext";
 import { axiosInstance } from "../../config";
 import { useNavigate } from 'react-router';
@@ -11,8 +10,23 @@ import ManageAside from '../../newComponents/manageAside';
 
 function ManageJob() {
     const { user } = useContext(AuthContext);
-    const [page, setPage] = useState(1);
     const [jobData, setJobData] = useState({})
+
+    console.log(user._id)
+
+    useEffect(() => {
+        const fetchJobs = async () => {
+          const res = await axiosInstance.get(`/jobs/userjobs/${user._id}`) 
+          console.log(res.data)
+        //   setJobData(
+        //     res.data.sort((p1, p2) => {
+        //       return new Date(p2.createdAt) - new Date(p1.createdAt);
+        //     }));
+        }
+        fetchJobs();
+      }, [user]);
+
+    console.log(jobData)
 
     return (
         <>
