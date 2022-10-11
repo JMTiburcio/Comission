d942cab8e5a78c5592d8bb7642a9f7caf892344a
JMTiburcio <jmrtme@gmail.com>
2022-10-11 18:47:21 -0300

    finishing styles for manageAside

--- client/src/newComponents/manageAside/index.jsx
+++ client/src/newComponents/manageAside/index.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import './styles.css';
+import CreateIcon from '@mui/icons-material/Create';
 
 function ManageAside({ }) {
   return (
     <aside className='manageAside'>
-      <a href="#">Post a free job</a>
-      <div>
-        <p>Save up to 35% by purchasing job posting budget in advance.</p>
-        <a href="#">Get discount</a>
-      </div>
-      <div>
-        <p>Billing information</p>
-        <a href="#">Payment method</a>
-        <a href="#">Purchase history</a>
+      <div className='manageAside__wrapper'>
+        <a className='manageAside__post' href="#">
+          <CreateIcon style={{fontSize:14, marginRight:5}}/>
+          Post a free job
+        </a>
+        <div className='manageAside__disc'>
+          <p>Save up to 35% by purchasing job posting budget in advance.</p>
+          <a className='manageAside__discLink' href="#">Get discount</a>
+        </div>
+        <div className='manageAside__disc'>
+          <p>Billing information</p>
+          <a className='manageAside__discLink' href="#">Payment method</a>
+          <a className='manageAside__discLink' href="#">Purchase history</a>
+        </div>
       </div>
     </aside>
   );
