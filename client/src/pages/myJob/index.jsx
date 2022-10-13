ea3b3c0c090644d80954880bb991dae734335a23
JMTiburcio <jmrtme@gmail.com>
2022-10-12 22:03:54 -0300

    creating components for myJob's page

--- client/src/pages/myJob/index.jsx
+++ client/src/pages/myJob/index.jsx
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import './styles.css';
+import { axiosInstance } from "../../config";
 import TopBar from '../../components/topbar'
-import ManageMenu from '../../newComponents/manageMenu';
-import ManageContainer from '../../newComponents/manageContainer';
-import ManageAside from '../../newComponents/manageAside';
+import MyJobTopCard from '../../newComponents/myJobTopCard';
+import MyJobContainer from '../../newComponents/myJobContainer';
+import MyJobAside from '../../newComponents/myJobAside';
 
 function MyJob() {
     const jobId = useParams().jobId;
+    const [job, setJob] = useState({})
+
+    useEffect(() => {
+        const fetchJobs = async () => {
+            try {
+                const res = await axiosInstance.get(`/jobs/${jobId}`) 
+                setJob(res.data);
+            } catch(err) {
+                console.log(err)
+            }
+        }
+        fetchJobs();
+    }, [jobId]);
+
 
     return (
         <>
         <TopBar/>
         <main className='myJob'>
-            <ManageMenu />
-            <ManageContainer />
-            <ManageAside />
+            <MyJobTopCard job={job}/>
+            <section className='myJob__container'>
+                <MyJobContainer />
+                <MyJobAside />
+            </section>
         </main>
         </>
     )
