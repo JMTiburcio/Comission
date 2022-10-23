213eb205fec172848e73b67b3823561c0daa5bc7
JMTiburcio <jmrtme@gmail.com>
2022-10-23 14:24:08 -0300

    typo error

--- client/src/pages/jobs/index.jsx
+++ client/src/pages/jobs/index.jsx
@@ -12,7 +12,7 @@ function Jobs() {
 
   useEffect(() => {
     const fetchJobs = async () => {
-      const res = await axiosInstance.get("/jobs//all/search/") 
+      const res = await axiosInstance.get("/jobs/all/search/") 
       setJobs(
         res.data.sort((p1, p2) => {
           return new Date(p2.createdAt) - new Date(p1.createdAt);
