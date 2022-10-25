71ed5f2af1417ab319ede2c4c3769f039edbeb23
JMTiburcio <jmrtme@gmail.com>
2022-10-25 17:51:30 -0300

    filter and dropdown on manage items page

--- client/src/newComponents/manageContainer/index.jsx
+++ client/src/newComponents/manageContainer/index.jsx
@@ -7,6 +7,7 @@ import ManageItem from '../manageItem';
 function ManageContainer({ page }) {
   const { user } = useContext(AuthContext);
   const [jobData, setJobData] = useState([])
+  const [filter, setFilter] = useState("Open")
   // console.log(user._id)
 
   useEffect(() => {
@@ -34,15 +35,30 @@ function ManageContainer({ page }) {
         <h1 className='manageContainer__header'>Posted Jobs</h1>
         <div className='manageContainer__filterBar'>
           <ul className='manageContainer__filterList'>
-            <li><button className='manageContainer__button'>Open</button></li>
-            <li><button className='manageContainer__button'>Draft</button></li>
-            <li><button className='manageContainer__button'>Close</button></li>
+            <li><button 
+              className={"manageContainer__filter"+(filter === "Open" ? '--active' : '')}
+              onClick={() => setFilter("Open")}
+            >
+              Open
+            </button></li>
+            <li><button 
+              className={"manageContainer__filter"+(filter === "Draft" ? '--active' : '')}
+              onClick={() => setFilter("Draft")}
+            >
+              Draft
+            </button></li>
+            <li><button 
+              className={"manageContainer__filter"+(filter === "Close" ? '--active' : '')}
+              onClick={() => setFilter("Close")}
+            >
+              Close
+            </button></li>
           </ul>
         </div>
         <ul className='manageContainer__resultList'>
           {jobData.map(job => (
             <li key={job._id}>
-              <ManageItem job={job} user={user} jobData={jobData} setJobData={setJobData} page={page}/>
+              <ManageItem job={job} user={user} jobData={jobData} setJobData={setJobData} filter={filter}/>
             </li>  
           ))}
         </ul>
