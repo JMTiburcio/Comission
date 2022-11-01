325f6704fb7699c4c1f9bf468e3266e5a68aabce
JMTiburcio <jmrtme@gmail.com>
2022-11-01 16:46:29 -0300

    solving bug of crash when failling auth

--- routes/auth.js
+++ routes/auth.js
@@ -28,12 +28,20 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
-    !user && res.status(404).json("user not found");
+    
+    if(user){
+      const validPassword = await bcrypt.compare(req.body.password, user.password)
+      
+      if(validPassword) {
+        res.status(200).json(user)
+      } else {
+        res.status(400).json("wrong password")
+      }
 
-    const validPassword = await bcrypt.compare(req.body.password, user.password)
-    !validPassword && res.status(400).json("wrong password")
+    } else {
+      res.status(404).json("user not found");
+    }
 
-    res.status(200).json(user)
   } catch (err) {
     res.status(500).json(err)
   }
