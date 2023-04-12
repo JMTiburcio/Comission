341ee3f8d8a8ea4c8deb839971d279af2ac46407
JMTiburcio <jmrtme@gmail.com>
2023-04-12 18:30:24 -0300

    chore: toggle comment

--- index.js
+++ index.js
@@ -5,24 +5,24 @@ const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const multer = require("multer");
-// const userRoute = require("./routes/users");
-// const companyRoute = require("./routes/companies");
-// const authRoute = require("./routes/auth");
-// const postRoute = require("./routes/posts");
-// const jobRoute = require("./routes/jobs");
-// const applyRoute = require("./routes/apply");
+const userRoute = require("./routes/users");
+const companyRoute = require("./routes/companies");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+const jobRoute = require("./routes/jobs");
+const applyRoute = require("./routes/apply");
 const path = require("path");
 const cors = require("cors");
 
 dotenv.config();
 
-// mongoose.connect(
-//   process.env.MONGO_URL,
-//   { useNewUrlParser: true, useUnifiedTopology: true },
-//   () => {
-//     console.log("Connected to MongoDB");
-//   }
-// );
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
 
 app.use("/images", express.static(path.join(__dirname, "public/images")));
 
@@ -50,18 +50,18 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
   }
 });
 
-// app.use("/api/auth", authRoute);
-// app.use("/api/users", userRoute);
-// app.use("/api/companies", companyRoute);
-// app.use("/api/posts", postRoute);
-// app.use("/api/jobs", jobRoute);
-// app.use("/api/apply", applyRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/companies", companyRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/jobs", jobRoute);
+app.use("/api/apply", applyRoute);
 
-// app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "/client/build", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build", "index.html"));
+});
 
 app.get("/", (req, res) => {
   res.send(`Backend is running: ${process.env.MONGO_URL}`);
